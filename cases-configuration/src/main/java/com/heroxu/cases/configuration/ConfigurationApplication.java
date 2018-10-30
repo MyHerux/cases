616e2944cc5ef76436a18138cd3a573a9b6cdee5
hua.xu <1013299500@qq.com>
2018-10-30 10:21:55 +0800

    init

---
+++ cases-configuration/src/main/java/com/heroxu/cases/configuration/ConfigurationApplication.java
@@ -0,0 +1,12 @@
+package com.heroxu.cases.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigurationApplication.class, args);
+    }
+}
---
+++ cases-configuration/src/test/java/com/heroxu/casesconfiguration/CasesConfigurationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.heroxu.casesconfiguration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CasesConfigurationApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
